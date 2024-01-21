121b5712bd015d0abfd927c2f3589d7f7d0200d5
OntologicalShock <58638318+OntologicalShock@users.noreply.github.com>
2024-01-21 21:52:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#Password Generator Project
+import random
+import sys
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+# numChar = int(input(f"How many characters long would you like your password to be?\n"))
+numLetter = int(input("How many letters would you like in your password?\n")) 
+numSymbol = int(input("How many symbols would you like?\n"))
+numNumber = int(input("How many numbers would you like?\n"))
+ezPass = str("")
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+for _i in range(numLetter):
+	ezPass += random.choice(letters)
+for _i in range(numSymbol):
+	ezPass += random.choice(symbols)
+for _i in range(numNumber):
+	ezPass += random.choice(numbers)
+
+print(f"Easy mode Password: {ezPass}")
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+ezPassList = list(ezPass)
+hardPass = str("")
+
+random.shuffle(ezPassList)
+hardPass = "".join(ezPassList)
+
+print(f"Hard Mode Password: {hardPass}")
+
+sys.exit()
---
+++ notes.md
@@ -0,0 +1,14 @@
+## Description of Program
+This is a Python program that generates a random password based on user input. It first asks the user how many letters, symbols, and numbers they want in their password. Then, it creates an "easy mode" password where the characters are added in the order specified by the user, and a "hard mode" password where the characters are shuffled into a random order. Finally, it prints out both passwords and exits the program. This program uses the random module to select characters from the predefined lists of letters, numbers, and symbols and the sys module to exit the program at the end.
+
+## Methods
+I made use of:
+- random
+- for loops
+- ranges
+- f-strings
+- join
+- sys
+
+## Lessons Learned
+I needed to 'Google' a fair bit to get a handle on join and random. By Google, I mean use the replit AI. The forked replit stopped working no matter what I did to the code (even commenting it all out) so I had to copy it all over to this new repl.
